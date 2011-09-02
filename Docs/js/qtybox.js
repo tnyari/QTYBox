5f3d4a69b2f89f601d368c318933b6286bd3238d
Tamas Nyari <tnyari@gmail.com>
2011-09-02 15:33:45 +0100

    Bug fixes handling float numbers

--- Docs/js/qtybox.js
+++ Docs/js/qtybox.js
@@ -10,7 +10,7 @@ authors:
 requires:
 - MooTools 1.3.2
 
-provides: QTYBox
+provides: [QTYBox]
 
 ...
 */
@@ -30,9 +30,11 @@ var QTYBox = new Class({
 		showError:true,
 		hlColor:'#FFBBBB',
 		cboxClass:'QTYBox',
-		onChange:null,
-		onIncrase:null,
-		onDecrase:null
+		disableEdit:false,
+		
+		onChange:$empty,
+		onIncrase:$empty,
+		onDecrase:$empty
 		
 	},
 	
@@ -42,10 +44,10 @@ var QTYBox = new Class({
 		 this.setOptions(options);
 		 if ($(this.name)){
 			 this.qtyfield = $(this.name);
-			 this.qtyvalue = this.qtyfield.get('value');
 			 this.cbox = false;
 			 this.initController();
 			 this.init();
+			 this.qtyvalue = this.qtyfield.get('value');
 			 
 		 } else {
 			 this.qtyfield = this.createInput();
@@ -89,46 +91,86 @@ var QTYBox = new Class({
 		
 		
 		//bind key events
-		this.qtyfield.addEvent('keydown', function(event) { 
-			if (_this.options.onChange != null){_this.options.onChange(this);}
+		this.qtyfield.addEvent('keypress', function(event) { 
+			_this.options.onChange(this);
+			
 			if (event.key == 'up' || event.key == 'right'){
 				_this.incrase();
-				if (_this.options.onIncrase != null){_this.options.onIncrase(this);}
+				_this.options.onIncrase(this);
 			} else if (event.key == 'down' || event.key == 'left'){
 				_this.decrase();
-				if (_this.options.onDecrase != null){_this.options.onDecrase(this);}
+				_this.options.onDecrase(this);
+			} else {
+				_this.checkValue(event);			
 			}
+			
 		});
-	
 		
 	},
+	
+	
+	checkValue: function (event){
+		var regex = RegExp(/^[-+]?[0-9]+$/);
+    	this.qtyvalue = this.getValue();
+		if ((!regex.test(event.key) && event.key != 'backspace' && event.key != 'delete' && event.key != '-'  ) || this.options.disableEdit  ){
+			event.preventDefault();
+			this.error();}
+		else {
+			
+			if (event.key != 'backspace' && event.key != 'delete'){
+				event.preventDefault();
+				if (event.key == '-' && this.qtyvalue > 0 && !this.qtyvalue.test('-') && this.options.minValue.toFloat() < 0) { 
+					var newValue = event.key + this.qtyvalue;
+				} else if (event.key != '-'){
+					var newValue = this.qtyvalue + event.key;
+				} else if (this.qtyvalue == '') {
+					var newValue = event.key;
+				} else {
+					var newValue = this.qtyvalue;
+				}
+				
+				if (newValue.toFloat() > this.options.maxValue.toFloat() || (newValue.toFloat() < this.options.minValue.toFloat() && newValue.length >=  this.options.minValue.length)   ){
+					this.error();
+				} else {
+					if (newValue == '-' || newValue == ''){
+						this.setValue(newValue);
+					} else {
+						this.setValue(newValue.toFloat());
+					}
+				}
+			}
+			
+		}
+	},	
 
 	incrase: function (){
-		if (this.options.maxValue > Number(this.qtyfield.get('value')) ){
-			this.qtyfield.set('value',Number(this.qtyfield.get('value')) + this.options.incValue);
-			//alert(JSON.encode(this.options.onChange));
-			if (this.options.onChange != null){this.options.onChange(this);}
-			if (this.options.onIncrase != null){this.options.onIncrase(this);}
+		this.qtyvalue = this.getValue();
+		var newValue = this.qtyvalue.toFloat() + this.options.incValue.toFloat();
+		if (this.options.maxValue >= newValue ){
+			this.setValue(newValue);
+			this.options.onChange(this);
+			this.options.onIncrase(this);
 		} else {
-			var bg_color=this.qtyfield.getStyle('backgroundColor');
-			if (this.options.showError){this.qtyfield.highlight(this.options.hlColor,bg_color)}
+			this.error();
 		}
-		
-		
 	},
 	decrase: function (){
-		if (this.options.minValue < Number(this.qtyfield.get('value')) ){
-			this.qtyfield.set('value',Number(this.qtyfield.get('value')) - this.options.incValue);
-			if (this.options.onChange != null){this.options.onChange(this);}
-			if (this.options.onDecrase != null){this.options.onDecrase(this);}
+		this.qtyvalue = this.getValue();
+		var newValue = this.qtyvalue.toFloat() - this.options.incValue.toFloat();
+		if (this.options.minValue <= newValue ){
+			this.setValue(newValue);
+			this.options.onChange(this);
+			this.options.onDecrase(this);
 		} else {
-			var bg_color=this.qtyfield.getStyle('backgroundColor');
-			if (this.options.showError){this.qtyfield.highlight(this.options.hlColor,bg_color)}
+			this.error();
 		}
-		
+	},
+	error:function(){
+		var bg_color=this.qtyfield.getStyle('backgroundColor').hexToRgb();
+		if (this.options.showError){this.qtyfield.highlight(this.options.hlColor,bg_color)}
 	},
 	getValue: function(){
-		this.qtyfield.get('value');
+		return this.qtyfield.get('value');
 	},
 	setValue: function(value){
 		this.qtyfield.set('value',value);
@@ -136,6 +178,7 @@ var QTYBox = new Class({
 	//Init value
 	init: function(){
 		this.qtyfield.set('value',this.options.initValue);
+		this.oldValue = this.options.initValue;
 	},
 	createInput: function(){
 		var qtybox =  new Element('input');
@@ -151,127 +194,3 @@ var QTYBox = new Class({
 	
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-
-
-
-
-
-
-var QTYBox = new Class({
-	
-	Implements: [Options],
-	options: {
-		place: 'after',
-		initValue: 1,
-		minValue:1,
-		maxValue:20,
-		incValue:1
-		
-	},
-	
-	
-	 initialize: function(name,options) {
-		 this.name = name; // A vezérlő neve
-		 this.setOptions(options);
-		 if ($(this.name)){
-			 this.qtyfield = $(this.name);
-			 this.qtyvalue = this.qtyfield.get('value');
-			 this.cbox = false;
-			 this.initController();
-			 this.init();
-		 } else {
-			 this.qtyfield = this.createInput();
-			 this.qtyvalue = this.options.initValue;
-			 this.cbox = new Element('div',{class:'QTYBox'});
-			 this.cbox.appendChild(this.qtyfield);
-			 this.initController();
-			 return this.cbox;
-		 }
-		 
-	},
-	initController: function() {
-		//Create controller
-		var _this = this;
-		this.maindiv = new Element('div', { 'class': 'arrowcontainer', id: this.name+'_arrowcontainer' });
-		this.updiv = new Element('div', { 'class': 'up', id: this.name+'_up' });
-		this.downdiv = new Element('div', { 'class': 'down', id: this.name+'_down' });
-		
-		this.updiv.inject(this.maindiv);
-		this.downdiv.inject(this.maindiv);
-		if (this.cbox){
-			this.cbox.appendChild(this.maindiv);
-		}else{
-			this.maindiv.inject(this.qtyfield,this.options.place);
-		}
-		//bind events
-		this.updiv.addEvent('click', this.incrase.bind(this));
-		this.downdiv.addEvent('click', this.decrase.bind(this));			
-		//bind events for interactivity
-		this.updiv.addEvent('mouseover', function(){_this.updiv.set('class','up_h');});
-		this.updiv.addEvent('mouseout', function(){_this.updiv.set('class','up');});
-		
-		this.updiv.addEvent('mousedown', function(){_this.updiv.set('class','up_c');});
-		this.updiv.addEvent('mouseup', function(){_this.updiv.set('class','up');});
-		
-		this.downdiv.addEvent('mouseover', function(){_this.downdiv.set('class','down_h');});
-		this.downdiv.addEvent('mouseout', function(){_this.downdiv.set('class','down');});
-		
-		this.downdiv.addEvent('mousedown', function(){_this.downdiv.set('class','down_c');});
-		this.downdiv.addEvent('mouseup', function(){_this.downdiv.set('class','down');});
-		
-		
-		//bind key events
-		this.qtyfield.addEvent('keydown', function(event) { 
-			if (event.key == 'up' || event.key == 'right'){
-				_this.incrase();
-			} else if (event.key == 'down' || event.key == 'left'){
-				_this.decrase();
-			}
-		});
-	
-		
-	},
-
-	incrase: function (){
-		if (this.options.maxValue > Number(this.qtyfield.get('value')) ){
-			this.qtyfield.set('value',Number(this.qtyfield.get('value')) + this.options.incValue);
-		}
-	},
-	decrase: function (){
-		if (this.options.minValue < Number(this.qtyfield.get('value')) ){
-			this.qtyfield.set('value',Number(this.qtyfield.get('value')) - this.options.incValue);
-		}
-	},
-	init: function(){
-		if (this.qtyfield.get('value') < 1 ){
-			this.qtyfield.set('value',1);
-		}
-	},
-	createInput: function(){
-		var qtybox =  new Element('input');
-		qtybox.set('id',this.name);	
-		qtybox.set('name',this.name);
-		qtybox.set('value',this.options.initValue);
-		//var cbox = new Element('div');
-		return qtybox;	
-					
-	},
-	getBox: function(){
-		return this.cbox;
-	}
-	
-});*/
